30b27393d563b8e82f8317e0c33abd0e611d315d
ArnaudGansmandel <a.gansmandel@laposte.net>
2024-08-15 14:49:25 +0200

    change folder

--- src/data_pipeline/preprocessing/data_processing.py
+++ src/data_pipeline/preprocessing/data_processing.py
@@ -1,4 +1,5 @@
 import os
+from data_pipeline.preprocessing.encoder import rgb_to_label
 import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
@@ -6,30 +7,6 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array  # type:
 
 IMG_SIZE = 224
 NUM_CLASSES = 21
-
-VOC_COLORMAP = np.array([
-    [0, 0, 0],
-    [128, 0, 0],
-    [0, 128, 0],
-    [128, 128, 0],
-    [0, 0, 128],
-    [128, 0, 128],
-    [0, 128, 128],
-    [128, 128, 128],
-    [64, 0, 0],
-    [192, 0, 0],
-    [64, 128, 0],
-    [192, 128, 0],
-    [64, 0, 128],
-    [192, 0, 128],
-    [64, 128, 128],
-    [192, 128, 128],
-    [0, 64, 0],
-    [128, 64, 0],
-    [0, 192, 0],
-    [128, 192, 0],
-    [0, 64, 128],
-])
 
 def preprocess_image(image_path: str) -> np.ndarray:
     """
@@ -46,23 +23,6 @@ def preprocess_image(image_path: str) -> np.ndarray:
     image /= 255.0  # Normalize to [0, 1]
     return image
 
-def rgb_to_label(mask: np.ndarray, colormap: np.ndarray) -> np.ndarray:
-    """
-    Convert an RGB mask to a label mask using a colormap.
-
-    Parameters:
-    mask (np.ndarray): RGB mask as a NumPy array.
-    colormap (np.ndarray): Colormap for converting RGB to labels.
-
-    Returns:
-    np.ndarray: Label mask as a NumPy array.
-    """
-    mask = np.array(mask)
-    label_mask = np.zeros((mask.shape[0], mask.shape[1]), dtype=np.int32)
-    for i, color in enumerate(colormap):
-        label_mask[np.all(mask == color, axis=-1)] = i
-    return label_mask
-
 def preprocess_mask(mask_path: str) -> tf.Tensor:
     """
     Load and preprocess a mask.
@@ -75,7 +35,7 @@ def preprocess_mask(mask_path: str) -> tf.Tensor:
     """
     mask = tf.keras.preprocessing.image.load_img(mask_path, target_size=(IMG_SIZE, IMG_SIZE))
     mask = tf.keras.preprocessing.image.img_to_array(mask)
-    label_mask = rgb_to_label(mask, VOC_COLORMAP)
+    label_mask = rgb_to_label(mask)
     return tf.expand_dims(label_mask, axis=-1)
 
 def load_and_preprocess_data(image_dir: str, mask_dir: str) -> tuple:
