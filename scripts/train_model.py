767e128d0fd45cbeb31bcac890b1686d506ac0d5
ArnaudGansmandel <a.gansmandel@laposte.net>
2024-08-15 14:51:39 +0200

    add polydecay and early stopping

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
-from src.deep_lab.data_processing import create_tf_dataset, load_and_preprocess_data
+from tensorflow.keras.callbacks import EarlyStopping
+from data_pipeline.preprocessing.data_processing import create_tf_dataset, load_and_preprocess_data
+from deep_lab.learning_rate import PolyDecay
 
 
-def train_and_save_model(model_to_train, path_to_save_model):
+def train_and_save_model(model_to_train, path_to_save_model=None):
     # Step 1: Preprocess the data
     image_dir = r'D:\01_Arnaud\Etudes\04_CNAM\RCP209\Projet\DeepLab\data\VOCdevkit\VOC2012\SegmentationClass'
     mask_dir = r'D:\01_Arnaud\Etudes\04_CNAM\RCP209\Projet\DeepLab\data\VOCdevkit\VOC2012\SegmentationObject'
@@ -14,15 +16,25 @@ def train_and_save_model(model_to_train, path_to_save_model):
     val_dataset = create_tf_dataset(val_images, val_masks)
 
     # Step 3: Train the model
-    model = model_to_train
-    model.compile(optimizer='adam', 
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    history = model.fit(train_dataset, validation_data=val_dataset, epochs=5)
+    initial_lr = 0.001
+    epochs = 50
+    poly_decay = PolyDecay(initial_learning_rate=initial_lr, max_epochs=epochs)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=poly_decay, weight_decay=0.0005)
 
-    model.save(path_to_save_model)
+    model = model_to_train
+    model.compile(optimizer=optimizer, 
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy', tf.keras.metrics.MeanIoU(num_classes=21)])
+
+    # Définition de l'arrêt anticipé (early stopping)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+    history = model.fit(train_dataset, validation_data=val_dataset, epochs=5, callbacks=[early_stopping])
+
+    if path_to_save_model is not None:
+        model.save(path_to_save_model)  
 
 if __name__ == "__main__":
     from deep_lab.model import DeepLabV3Plus
 
-    train_and_save_model(DeepLabV3Plus, r'D:\01_Arnaud\Etudes\04_CNAM\RCP209\Projet\CodeV3\deep-lab2\saved_model\my_model.h5')
+    train_and_save_model(DeepLabV3Plus(dropout_rate=0.3), r'D:\01_Arnaud\Etudes\04_CNAM\RCP209\Projet\DeepLab\results\models\deep_lab_v3_plus_model.h5')
