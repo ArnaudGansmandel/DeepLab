308f367f6a78092a37421f4c46e60c67bb45fa31
ArnaudGansmandel <a.gansmandel@laposte.net>
2024-08-17 00:17:00 +0200

    fix low_level_feature that was a keras tensor. Now is a functionnal API

--- src/deep_lab/model.py
+++ src/deep_lab/model.py
@@ -11,37 +11,27 @@ class DeepLabV3Plus(Model):
             name="DeepLabV3Plus",
             **kwargs
         ):
-        super(DeepLabV3Plus, self).__init__(name=name, **kwargs)
+        super().__init__(name=name, **kwargs)
         self.backbone = Backbone(dropout_rate=dropout_rate)
         self.aspp = ASPP(dropout_rate=dropout_rate)
         self.decoder = Decoder(dropout_rate=dropout_rate)
 
-    def call(self, inputs, training=False):
-        x = self.backbone(inputs, training=training)
+    def call(self, inputs, training=None):
+        x, low_level_feature = self.backbone(inputs, training=training)
         x = self.aspp(x, training=training)
-        low_level_feature = self.backbone.resnet_model.get_layer('conv2_block3_out').output  
-        low_level_feature = tf.keras.layers.Lambda(lambda x: tf.stop_gradient(x))(low_level_feature)  # Avoid backpropagation
         x = self.decoder(x, low_level_feature, training=training)
         return x
-
+    
 # Example usage
 if __name__ == "__main__":
     IMG_SIZE = 224
     NUM_CLASSES = 21
     input = tf.keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3))    
 
-    print("DeepLabV3Plus in training mode")
+    print("DeepLabV3Plus summary")
     deeplab_model_training = DeepLabV3Plus()
-    output = deeplab_model_training(input, training=True)
+    output = deeplab_model_training(input)
 
     deeplab_model_training = Model(inputs=input, outputs=output)
     deeplab_model_training.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
     deeplab_model_training.summary()
-
-    print("DeepLabV3Plus in inference mode")
-    deeplab_model_inference = DeepLabV3Plus()
-    output = deeplab_model_inference(input, training=False)
-
-    deeplab_model_inference = Model(inputs=input, outputs=output)
-    deeplab_model_inference.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-    deeplab_model_inference.summary()
