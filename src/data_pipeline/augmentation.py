863883adba6b50e309812014dc3abbbd602eac3a
ArnaudGansmandel <a.gansmandel@laposte.net>
2024-08-21 00:39:07 +0200

    remove resize_and_rescale bc already in preprocess

--- src/data_pipeline/augmentation.py
+++ src/data_pipeline/augmentation.py
@@ -4,16 +4,11 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 class DataAugmentation:
-    def __init__(self, img_size=224):
-        self.img_size = img_size
+    def __init__(self, config):
+        self.img_size = config['img_size']
         self.data_augmentation = tf.keras.Sequential([
             layers.RandomFlip("horizontal_and_vertical"),
             layers.RandomRotation(0.2),
-        ])
-
-        self.resize_and_rescale = tf.keras.Sequential([
-            layers.Resizing(self.img_size, self.img_size),
-            layers.Rescaling(1./255)
         ])
 
     def augment(self, image: tf.Tensor, mask: tf.Tensor) -> (tf.Tensor, tf.Tensor): # type: ignore
