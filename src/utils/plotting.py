323ef5dc90663daf11d4f05a18c1bafc1aa54a22
ArnaudGansmandel <a.gansmandel@laposte.net>
2024-08-21 11:18:45 +0200

    fix import

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,4 +1,4 @@
-from data_pipeline.preprocessing.data_processing import preprocess_image, preprocess_mask
+from data_pipeline.processing import preprocess_image, preprocess_mask
 from matplotlib import pyplot as plt
 import numpy as np
 import tensorflow as tf
