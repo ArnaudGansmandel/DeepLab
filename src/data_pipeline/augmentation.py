6ec924d803a93df53526f267aa57340621592f45
ArnaudGansmandel <a.gansmandel@laposte.net>
2024-08-22 01:06:29 +0200

    implement multi scale crops

--- src/data_pipeline/augmentation.py
+++ src/data_pipeline/augmentation.py
@@ -1,29 +1,26 @@
-# src/data_pipeline/augmentation.py
-
 import tensorflow as tf
 from tensorflow.keras import layers
 
 class DataAugmentation:
-    def __init__(self, config):
-        self.img_size = config['img_size']
-        self.data_augmentation = tf.keras.Sequential([
-            layers.RandomFlip("horizontal_and_vertical"),
-            layers.RandomRotation(0.2),
-        ])
+    def __init__(self):
+        # Store augmentation pipelines in a list
+        self.augmentations = [
+            tf.keras.Sequential([
+                layers.RandomFlip("horizontal_and_vertical"),
+                layers.RandomRotation(0.2),
+            ]),
+            tf.keras.Sequential([
+                layers.Resizing(448, 448),
+                layers.RandomCrop(224, 224)
+            ]),
+            tf.keras.Sequential([
+                layers.Resizing(672, 672),
+                layers.RandomCrop(224, 224)
+            ]),
+            tf.keras.Sequential([
+                layers.Resizing(896, 896),
+                layers.RandomCrop(224, 224)
+            ])
+        ]
 
-    def augment(self, image: tf.Tensor, mask: tf.Tensor) -> (tf.Tensor, tf.Tensor): # type: ignore
-        """
-        Apply data augmentation to an image and its corresponding mask.
 
-        Parameters:
-        image (tf.Tensor): Input image.
-        mask (tf.Tensor): Corresponding mask.
-
-        Returns:
-        tuple: Augmented image and mask.
-        """
-        # Apply augmentation to both image and mask
-        image = self.data_augmentation(image)
-        mask = self.data_augmentation(mask)
-
-        return image, mask
