d908aba88d693f7283be88e05b695161d589dc03
ArnaudGansmandel <a.gansmandel@laposte.net>
2024-09-06 23:33:30 +0200

    dealing with freezing

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,4 +1,5 @@
-from deep_lab.model import DeepLabV3Plus
+# scripts\train_model.py
+from deep_lab.models.model import DeepLabV3Plus
 from data_pipeline.data_loader import DataLoader
 import tensorflow as tf
 from training.trainer import Trainer
@@ -7,8 +8,8 @@ from utils.plotting import show_predictions
 # Training configuration
 config = {
     'learning_rate': 0.007,
-    'fine_tuning_learning_rate': 0.001,
-    'epochs': 3,
+    'fine_tuning_learning_rate': 0.00001,
+    'epochs': 2,
     'batch_size': 16,
     'num_classes': 21,
     'img_size': 224,
@@ -27,7 +28,7 @@ if __name__ == "__main__":
     trainval_dataset = data_loader.load_data('trainval', augment=False)
 
     # Create the model
-    model = DeepLabV3Plus(ouput_stride=16)
+    model = DeepLabV3Plus(output_stride=16)
 
     # Create a dummy input tensor with the shape (batch_size, height, width, channels)
     dummy_input = tf.random.normal((1, config['img_size'], config['img_size'], 3))
@@ -36,19 +37,25 @@ if __name__ == "__main__":
     _ = model(dummy_input)
 
     # Freeze the ResNet backbone
-    model.backbone.resnet_model.trainable = False
+    # model.backbone.resnet_model.trainable = False
+    # model.resnet_model.trainable = False
+
+    for layers in model.backbone.resnet_model.layers:
+        layers.trainable = False
+
+    # for layers in model.backbone.backbone.layers:
+    #     layers.trainable = False
+
+    # model.backbone.backbone.trainable = False
 
     # Create a Trainer instance
     trainer = Trainer(model=model, train_dataset=trainaug_dataset, val_dataset=val_dataset, config=config)
 
     # Load model from checkpoint if available
-    trainer.load_from_checkpoint()
+    #trainer.load_from_checkpoint()
 
     # Train the model
     trainer.train()
-
-    # Show some predictions
-    show_predictions(trainer.model, dataset=val_dataset, num=3)
     
     # Save the final model
     trainer.model.save_weights('results/models/top_model.weights.h5')
