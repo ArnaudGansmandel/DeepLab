3988eb291eb2ba8fe3704942439428090c09af20
ArnaudGansmandel <a.gansmandel@laposte.net>
2024-09-06 23:31:40 +0200

    remove shuffleling + add visualization during dataset creation

--- src/data_pipeline/data_loader.py
+++ src/data_pipeline/data_loader.py
@@ -1,8 +1,11 @@
+# src\data_pipeline\data_loader.py
 import os
 import pathlib
+from matplotlib import pyplot as plt
 import tensorflow as tf
 from data_pipeline.preprocessing import Preprocessor
 from data_pipeline.augmentation import DataAugmentation
+from utils.plotting import display
 
 class DataLoader:
     def __init__(self, config):
@@ -22,8 +25,8 @@ class DataLoader:
             """
             image_dir = pathlib.Path(os.path.join('data', dir_path, 'images'))
             mask_dir = pathlib.Path(os.path.join('data', dir_path, 'masks'))
-            image_list_ds = tf.data.Dataset.list_files(str(image_dir/'*'))
-            mask_list_ds = tf.data.Dataset.list_files(str(mask_dir/'*'))
+            image_list_ds = tf.data.Dataset.list_files(str(image_dir/'*'), shuffle=False)
+            mask_list_ds = tf.data.Dataset.list_files(str(mask_dir/'*'), shuffle=False)
 
             return image_list_ds, mask_list_ds
 
@@ -62,13 +65,18 @@ class DataLoader:
                                                num_parallel_calls=tf.data.AUTOTUNE)
         mask_dataset = mask_list_ds.map(lambda x: self.preprocessor.preprocess_mask(x),
                                              num_parallel_calls=tf.data.AUTOTUNE)
-        dataset = tf.data.Dataset.zip((image_dataset, mask_dataset))
-
+        dataset = tf.data.Dataset.zip((image_dataset, mask_dataset))  
+        # for image, mask in dataset.take(1):
+        #     display([image, mask])
         if augment:
             augmented_datasets = []
             for aug in self.augmentor.augmentations:
-                augmented_dataset = dataset.map(lambda x, y: (aug(x), tf.cast(aug(y), tf.int32)),
+                augmented_dataset = dataset.map(lambda x, y: aug(x, y),
                                                 num_parallel_calls=tf.data.AUTOTUNE)
+
+                # for image, mask in augmented_dataset.take(1):
+                #     display([image, mask])                
+
                 augmented_datasets.append(augmented_dataset)
             for aug_ds in augmented_datasets:
                 dataset = dataset.concatenate(aug_ds)
@@ -76,3 +84,21 @@ class DataLoader:
         dataset = self.__configure_for_performance(dataset)
 
         return dataset
+    
+# Example usage
+if __name__ == "__main__":
+    # Training configuration
+    config = {
+        'learning_rate': 0.007,
+        'fine_tuning_learning_rate': 0.001,
+        'epochs': 3,
+        'batch_size': 16,
+        'num_classes': 21,
+        'img_size': 224,
+        'checkpoint_filepath': 'results/checkpoints/model.weights.h5',
+        'model_save_filepath': 'results/models/top_model.weights.h5',
+    }
+
+    data_loader = DataLoader(config)
+    train_dataset = data_loader.load_data('train', augment=True)
+    #val_dataset = data_loader.load_data('val')
